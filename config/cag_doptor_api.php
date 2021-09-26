d0e82d5d885d9faaa33c16a2b8b859aa954a40b5
Robin <faisalrobin22@gmail.com>
2021-09-26 16:25:11 +0600

    cag doptor api config

---
+++ config/cag_doptor_api.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'auth' => [
+        'client_login_url' => env('API_URL_DOPTOR', '') . '/client/login',
+        'client_id' => env('DOPTOR_CLIENT_ID', ''),
+        'client_pass' => env('DOPTOR_CLIENT_PASS', ''),
+    ],
+    'widget' => env('API_URL_DOPTOR', '') . '/switch/widget',
+
+    'offices' => env('API_URL_DOPTOR', '') . '/offices',
+    'office_wise_designation' => env('API_URL_DOPTOR', '') . '/office/get-office-wise-designation',
+    'office_and_grade_wise_designation' => env('API_URL_DOPTOR', '') . '/office/get-office-and-grade-wise-designation',
+    'office_unit_designation_map' => env('API_URL_DOPTOR', '') . '/office/unit-designation-map',
+    'office_unit_designation_employee_map' => env('API_URL_DOPTOR', '') . '/office/unit-designation-employee-map',
+    'designation_role' => env('API_URL_DOPTOR', '') . '/office/designation-role',
+];
