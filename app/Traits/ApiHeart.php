a70356df0ac24c8340bc0e8c00d355e928e6ca09
theeMahmud <mahmudrajucse@gmail.com>
2021-09-26 16:21:34 +0600

    apiheart dop init

--- app/Traits/ApiHeart.php
+++ app/Traits/ApiHeart.php
@@ -4,6 +4,37 @@ namespace App\Traits;
 
 trait ApiHeart
 {
+    public function initDoptorHttp(): \Illuminate\Http\Client\PendingRequest
+    {
+        return Http::withHeaders($this->apiHeaders())->withToken($this->getDoptorToken($this->getUsername()));
+    }
 
+    public function getDoptorToken($username)
+    {
+        $url = config('cag_doptor_api.auth.client_login_url');
+        $client_id = config('cag_doptor_api.auth.client_id');
+        $client_pass = config('cag_doptor_api.auth.client_pass');
+
+        if (!session()->has('_doptor_token') || session('_doptor_token') == '') {
+            $token = $this->getClientToken($url, $client_id, $client_pass, $username);
+            session(['_doptor_token' => $token]);
+        }
+        return session('_doptor_token');
+    }
+
+    public function getClientToken($url, $client_id, $client_pass, $username = '')
+    {
+        if ($username == '') {
+            $getToken = $this->initHttp()->post($url, ['client_id' => $client_id, 'password' => $client_pass]);
+        } else {
+            $getToken = $this->initHttp()->post($url, ['client_id' => $client_id, 'password' => $client_pass, 'username' => $username,]);
+        }
+
+        if ($getToken->status() == 200 && $getToken->json()['status'] == 'success') {
+            return $getToken->json()['data']['token'];
+        } else {
+            return '';
+        }
+    }
 }
 
