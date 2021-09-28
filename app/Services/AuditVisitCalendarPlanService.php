ce6fa1ccd1535591b1d4734885735d50cfaed2e0
theeMahmud <mahmudrajucse@gmail.com>
2021-09-28 13:47:57 +0600

    individual calendar (1)

--- app/Services/AuditVisitCalendarPlanService.php
+++ app/Services/AuditVisitCalendarPlanService.php
@@ -46,9 +46,12 @@ class AuditVisitCalendarPlanService
     {
         $cdesk = json_decode($request->cdesk, false);
 
+        $office_db_con_response = $this->switchOffice($cdesk->office_id);
+        if (!isSuccessResponse($office_db_con_response)) {
+            return ['status' => 'error', 'data' => $office_db_con_response];
+        }
         try {
-            $calendar = AuditVisitCalenderPlanMember::with('plan_team')->where('team_member_designation_id', $cdesk->designation_id)->where('team_member_officer_id', $cdesk->officer_id)->where('team_member_office_id', $cdesk->office_id)->get();
-
+            $calendar = AuditVisitCalenderPlanMember::where('team_member_designation_id', $cdesk->designation_id)->where('team_member_officer_id', $cdesk->officer_id)->where('team_member_office_id', $cdesk->office_id)->get();
             return ['status' => 'success', 'data' => $calendar];
         } catch (\Exception $exception) {
             return ['status' => 'error', 'data' => $exception->getMessage()];
