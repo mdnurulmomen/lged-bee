1d846b9f7ce3a40053101ce6054c7910d9cda782
theeMahmud <mahmudrajucse@gmail.com>
2021-10-04 17:25:31 +0600

    audit plan team info

---
+++ database/migrations/2021_10_04_112008_create_audit_plan_team_infos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuditPlanTeamInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('BeeCoreDB')->create('audit_plan_team_infos', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('fiscal_year_id');
+            $table->bigInteger('duration_id');
+            $table->bigInteger('outcome_id');
+            $table->bigInteger('output_id');
+            $table->bigInteger('directorate_id');
+            $table->integer('total_teams');
+            $table->integer('total_team_members');
+            $table->integer('total_employees');
+            $table->integer('total_working_days');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('audit_plan_team_infos');
+    }
+}
