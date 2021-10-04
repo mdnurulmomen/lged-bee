6762ab487fa4efb85d835deababd19e7c1160bf7
Shamim kafi <shamimunbindelowarkafi@gmail.com>
2021-10-04 10:38:00 +0600

    stategic plan

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -10,7 +10,26 @@ class Document extends Model
     use HasFactory;
 
     protected $connection = 'OfficeDB';
-    protected $table = 'documents';
+    protected $fillable = [
+        'document_type',
+        'relational_id',
+        'fiscal_year',
+        'is_main',
+        'attachment_type',
+        'attachment_description',
+        'user_file_name',
+        'file_custom_name',
+        'file_location',
+        'file_url', 'file_dir',
+        'file_size_in_kb',
+        'content_cover',
+        'content_body',
+        'meta_data',
+        'created_by',
+        'modified_by'
+    ];
+
+
     public  $timestamps = false;
 
 }
