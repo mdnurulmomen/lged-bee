548047602e07592ae018cfe317f4ea0bc88c73db
Robin <faisalrobin22@gmail.com>
2021-10-12 19:45:00 +0600

    report bug

--- app/Services/MISAndDashboardService.php
+++ app/Services/MISAndDashboardService.php
@@ -49,10 +49,9 @@ class MISAndDashboardService
             return ['status' => 'error', 'data' => $office_db_con_response];
         }
 
-        $team = AuditVisitCalendarPlanTeam::selectRaw('count(id) as total_team, SUM(activity_man_days) AS total_working_days,duration_id,outcome_id,output_id')
-            ->where('fiscal_year_id', $request->fiscal_year_id)
-            ->groupBy('fiscal_year_id')
-            ->first();
+//        $team = AuditVisitCalendarPlanTeam::select('duration_id,outcome_id,output_id')->where('fiscal_year_id', $request->fiscal_year_id)->first();
+        $total_team = AuditVisitCalendarPlanTeam::where('fiscal_year_id', $request->fiscal_year_id)->count();
+        $total_activity_man_days = AuditVisitCalendarPlanTeam::where('fiscal_year_id', $request->fiscal_year_id)->sum('activity_man_days');
         $team_member_count = AuditVisitCalenderPlanMember::where('fiscal_year_id', $request->fiscal_year_id)->count();
         $this->emptyOfficeDBConnection();
 
@@ -61,16 +60,16 @@ class MISAndDashboardService
         AuditPlanTeamInfo::updateOrCreate(
             ['fiscal_year_id' => $request->fiscal_year_id],
             [
-                'duration_id' => $team->duration_id,
-                'outcome_id' => $team->outcome_id,
-                'output_id' => $team->output_id,
+                'duration_id' => 1,
+                'outcome_id' => 1,
+                'output_id' => 1,
                 'office_id' => $cdesk->office_id,
                 'office_name_bn' => $cdesk->office_name_bn,
                 'office_name_en' => $cdesk->office_name_en,
-                'total_teams' => $team->total_team,
+                'total_teams' => $total_team,
                 'total_employees' => $total_resources,
                 'total_team_members' => $team_member_count,
-                'total_working_days' => $team->total_working_days,
+                'total_working_days' => $total_activity_man_days,
 
             ]
         );
