13b864f339e0ee1809e8a5c7d8e18370e332e431
Robin <faisalrobin22@gmail.com>
2021-10-14 12:52:44 +0600

    refactor api

--- app/Http/Controllers/AuditVisitCalenderPlanController.php
+++ app/Http/Controllers/AuditVisitCalenderPlanController.php
@@ -7,20 +7,6 @@ use Illuminate\Http\Request;
 
 class AuditVisitCalenderPlanController extends Controller
 {
-    public function getVisitPlanCalendar(Request $request, AuditVisitCalendarPlanService $auditVisitCalendarPlanService): \Illuminate\Http\JsonResponse
-    {
-
-        $calendar_data = $auditVisitCalendarPlanService->getVisitPlanCalendar($request);
-
-        if (isSuccessResponse($calendar_data)) {
-            $response = responseFormat('success', $calendar_data['data']);
-        } else {
-            $response = responseFormat('error', $calendar_data['data']);
-        }
-
-        return response()->json($response);
-    }
-
     public function getTeamVisitPlanCalendar(Request $request, AuditVisitCalendarPlanService $auditVisitCalendarPlanService): \Illuminate\Http\JsonResponse
     {
         $individual_plan = $auditVisitCalendarPlanService->getTeamVisitPlanCalendar($request);
--- app/Services/AuditVisitCalendarPlanService.php
+++ app/Services/AuditVisitCalendarPlanService.php
@@ -13,29 +13,6 @@ class AuditVisitCalendarPlanService
 {
     use GenericData, ApiHeart;
 
-    public function getVisitPlanCalendar(Request $request): array
-    {
-        $cdesk = json_decode($request->cdesk, false);
-
-        $office_db_con_response = $this->switchOffice($cdesk->office_id);
-        if (!isSuccessResponse($office_db_con_response)) {
-            return ['status' => 'error', 'data' => $office_db_con_response];
-        }
-
-        try {
-            if ($cdesk->is_office_admin || $cdesk->is_office_head) {
-                $calendar = AuditVisitCalendarPlanTeam::with('plan_member')->paginate(20);
-            } else {
-                $calendar = AuditVisitCalenderPlanMember::with('plan_team')->where('team_member_designation_id', $cdesk->designation_id)->where('team_member_officer_id', $cdesk->officer_id)->where('team_member_office_id', $cdesk->office_id)->get();
-            }
-
-            return ['status' => 'success', 'data' => $calendar];
-        } catch (\Exception $exception) {
-            return ['status' => 'error', 'data' => $exception->getMessage()];
-        }
-
-    }
-
     public function getTeamVisitPlanCalendar(Request $request): array
     {
         $cdesk = json_decode($request->cdesk, false);
