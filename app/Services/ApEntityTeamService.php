b1f6a097ac0c886aa12977edadd5f99b14830697
Shamim kafi <shamimunbindelowarkafi@gmail.com>
2021-10-28 20:28:20 +0600

    test 01

--- app/Services/ApEntityTeamService.php
+++ app/Services/ApEntityTeamService.php
@@ -141,11 +141,9 @@ class ApEntityTeamService
     public function saveTeamSchedule($team_schedules, $audit_plan_id)
     {
         try {
+            dump($team_schedules);
             foreach ($team_schedules as $designation_id => $schedule_data) {
                 $team_data = AuditVisitCalendarPlanTeam::where('audit_plan_id', $audit_plan_id)->where('leader_designation_id', $designation_id)->first();
-                if ($designation_id == 413){
-                    dump($team_data);
-                }
                 if (!$team_data) {
                     throw new \Exception('Team is not formed');
                 }
