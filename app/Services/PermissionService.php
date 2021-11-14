b6cdcb0c12f93ebc7c45debc9c233ade0de18f46
theeMahmud <mahmudrajucse@gmail.com>
2021-11-14 19:31:55 +0600

    role, menu, module, permission init (3)

--- app/Services/PermissionService.php
+++ app/Services/PermissionService.php
@@ -6,6 +6,7 @@ use App\Models\PMenu;
 use App\Models\PMenuModule;
 use App\Models\PMenuModuleRoleMap;
 use App\Models\PMenuRoleMap;
+use App\Models\PRole;
 use App\Traits\ApiHeart;
 use App\Traits\GenericData;
 
@@ -44,13 +45,19 @@ class PermissionService
     public function permittedModuleWiseMenus($master_designation_id, $module_link): array
     {
         try {
-            $module = PMenuModule::where('link', $module_link)->first();
-            $roleMenuMapIds = PMenuRoleMap::where('role_id', $master_designation_id)->pluck('menu_id');
-            $menus = PMenu::where('menu_module_id', $module->id)->whereIn('id', $roleMenuMapIds)->with([
+            $module = PMenuModule::where('module_link', $module_link)->first();
+            $role = PRole::where('master_designation_id', $master_designation_id)->first();
+
+            $roleMenuMapIds = PMenuRoleMap::where('role_id', $role->id)->pluck('menu_id');
+            $menus = PMenu::where('module_menu_id', $module->id)->whereIn('id', $roleMenuMapIds)->with([
                 'children' => function ($query) use ($roleMenuMapIds) {
                     $query->whereIn('id', $roleMenuMapIds);
-                }])->where('parent_menu_id', '0')->get();
-            return ['status' => 'success', 'data' => $menus];
+                }])->where('parent_menu_id', null)->get();
+            $data = [
+                'module' => $module,
+                'menus' => $menus,
+            ];
+            return ['status' => 'success', 'data' => $data];
         } catch (\Exception $exception) {
             return ['status' => 'error', 'data' => $exception->getMessage()];
         }
