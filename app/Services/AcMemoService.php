48841394cfeadbb212eb55cb97a938d7196365c5
Robin <faisalrobin22@gmail.com>
2022-01-16 02:48:22 +0600

    bug

--- app/Services/AcMemoService.php
+++ app/Services/AcMemoService.php
@@ -553,16 +553,13 @@ class AcMemoService
 
     public function responseOfRpuMemo(Request $request): array
     {
-        return ['status' => 'success', 'data' => $request->all()];
         $office_db_con_response = $this->switchOffice($request->office_id);
         if (!isSuccessResponse($office_db_con_response)) {
             return ['status' => 'error', 'data' => $office_db_con_response];
         }
         try {
-            $ac_memo = AcMemo::find($request->memo_id);
-            $ac_memo->response_of_rpu = $request->response_of_rpu;
-            $ac_memo->save();
 
+            AcMemo::where('id',$request->memo_id)->update(['response_of_rpu' => $request->response_of_rpu]);
             return ['status' => 'success', 'data' => 'Response Send Successfully'];
 
         } catch (\Exception $exception) {
