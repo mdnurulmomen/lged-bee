afd4c54ebea98480b07646eeb31ae4806a9228f2
Shamim kafi <shamimunbindelowarkafi@gmail.com>
2022-01-15 21:35:56 +0600

    bug

--- app/Services/RpuAirReportService.php
+++ app/Services/RpuAirReportService.php
@@ -68,7 +68,7 @@ class RpuAirReportService
 //            return ['status' => 'success', 'data' => $send_air_data];
 
             $send_air_to_rpu = $this->initRPUHttp()->post(config('cag_rpu_api.send_air_to_rpu'), $send_air_data)->json();
-            return ['status' => 'success', 'data' => $send_air_to_rpu];
+            //return ['status' => 'success', 'data' => $send_air_to_rpu];
             if ($send_air_to_rpu['status'] == 'success') {
                 $air_info->is_sent = 1;
                 $air_info->save();
