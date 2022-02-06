623300e02611fc21f48cf7bb6fd85c55a81dd2ad
Robin <faisalrobin22@gmail.com>
2022-02-06 13:27:50 +0600

    paginate

--- config/bee_config.php
+++ config/bee_config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'per_page_pagination' => 40,
+    'per_page_pagination' => 200,
     'secret_key' => env('SECRET_KEY'),
     'is_keycloak_enabled' => true,
     'client_id' => 'bee-client',
