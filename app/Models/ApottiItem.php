97c4fd9f1aefb73377953425742cd84adf1fe953
Robin <faisalrobin22@gmail.com>
2022-03-01 16:29:24 +0600

    comment apotti status

--- app/Models/ApottiItem.php
+++ app/Models/ApottiItem.php
@@ -10,19 +10,19 @@ class ApottiItem extends Model
     use HasFactory;
     protected $connection = 'OfficeDB';
 
-    public static $memo_status_list = [
-        '0' => 'N/A',
-        '1' => 'নিস্পন্ন',
-        '2' => 'অনিস্পন্ন',
-        '3' => 'আংশিক নিস্পন্ন',
-    ];
-
-    protected $appends = ['memo_status_name'];
-
-    public function getMemoStatusNameAttribute()
-    {
-        return self::$memo_status_list[$this->attributes['memo_status']];
-    }
+//    public static $memo_status_list = [
+//        '0' => 'N/A',
+//        '1' => 'নিস্পন্ন',
+//        '2' => 'অনিস্পন্ন',
+//        '3' => 'আংশিক নিস্পন্ন',
+//    ];
+//
+//    protected $appends = ['memo_status_name'];
+//
+//    public function getMemoStatusNameAttribute()
+//    {
+//        return self::$memo_status_list[$this->attributes['memo_status']];
+//    }
 
     public function fiscal_year(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
