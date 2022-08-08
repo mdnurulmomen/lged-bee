920675a1b6cad08473f59ee947b206bf9d936f4c
Robin <faisalrobin22@gmail.com>
2022-08-08 18:04:35 +0600

    office order show api chage

--- app/Services/ApDcOfficerOrderService.php
+++ app/Services/ApDcOfficerOrderService.php
@@ -59,7 +59,8 @@ class ApDcOfficerOrderService
     public function showOfficeOrder(Request $request): array
     {
         $cdesk = json_decode($request->cdesk, false);
-        $office_db_con_response = $this->switchOffice($cdesk->office_id);
+        $office_id = $request->office_id ?  $request->office_id : $cdesk->office_id;
+        $office_db_con_response = $this->switchOffice($office_id);
         if (!isSuccessResponse($office_db_con_response)) {
             return ['status' => 'error', 'data' => $office_db_con_response];
         }
