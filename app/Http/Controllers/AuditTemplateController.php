e52276f7811ef601e66fa1d8a0e32e41fe9dc5df
DESKTOP-VUGJ8J1\DELL <shamimkafi123@gmail.com>
2022-09-04 13:38:32 +0600

    template bug

--- app/Http/Controllers/AuditTemplateController.php
+++ app/Http/Controllers/AuditTemplateController.php
@@ -9,10 +9,14 @@ class AuditTemplateController extends Controller
 {
     public function show(Request $request): \Illuminate\Http\JsonResponse
     {
-        $type = $request->template;
-        $lang = $request->language;
         try {
-            $template = AuditTemplate::select('content')->where('lang', $lang)->where('template_type', $type)->where('status', 1)->first()->toArray();
+            $template = AuditTemplate::select('content')
+                ->where('lang', $request->language)
+                ->where('template_type', $request->template_type)
+                ->where('template_name', $request->template_name)
+                ->where('status', 1)
+                ->first()
+                ->toArray();
             return response()->json(responseFormat('success', $template));
         } catch (\Exception $exception) {
             return response()->json(responseFormat('error', $exception));
