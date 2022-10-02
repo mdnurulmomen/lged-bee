6ef090f85a27dcb21f582a4b086a89ea06fa0971
theeMahmud <mahmudrajucse@gmail.com>
2022-10-02 15:58:32 +0600

    purging db switching

--- app/Traits/GenericData.php
+++ app/Traits/GenericData.php
@@ -22,33 +22,34 @@ trait GenericData
 
     public function switchOffice($office_id, $status = -1, $returnErrorMsg = true): array
     {
-        $officeDomain = OfficeDomain::where('office_id', $office_id)->first();
-
-        if ($status != -1) {
-            $officeDomain = OfficeDomain::where('status', $status)->first();
-        }
-        if (empty($officeDomain)) {
-            $msg = sprintf("অফিস ডাটাবেজ পাওয়া যায় নি! সাপোর্ট টিমের সাথে যোগাযোগ করুন। অফিস আইডিঃ %s.", $office_id);
-            return ['status' => 'error', 'message' => $msg];
-        }
-
-        $this->emptyOfficeDBConnection();
-
-        Config::set("database.connections.OfficeDB", [
-            'driver' => 'mysql',
-            "host" => $officeDomain->domain_host,
-            "database" => $officeDomain->office_db,
-            "username" => $officeDomain->domain_username,
-            "password" => $officeDomain->domain_password,
-            "port" => 3306,
-            'charset' => 'utf8',
-            'collation' => 'utf8_general_ci',
-        ]);
-
-        DB::purge('OfficeDB');
-        DB::reconnect('OfficeDB');
-
-        return ['status' => 'success', 'message' => __("Successfully Connected"), 'office_id' => $office_id, 'office_domain' => $officeDomain];
+        return ['status' => 'success', 'message' => __("Successfully Connected")];
+//        $officeDomain = OfficeDomain::where('office_id', $office_id)->first();
+//
+//        if ($status != -1) {
+//            $officeDomain = OfficeDomain::where('status', $status)->first();
+//        }
+//        if (empty($officeDomain)) {
+//            $msg = sprintf("অফিস ডাটাবেজ পাওয়া যায় নি! সাপোর্ট টিমের সাথে যোগাযোগ করুন। অফিস আইডিঃ %s.", $office_id);
+//            return ['status' => 'error', 'message' => $msg];
+//        }
+//
+//        $this->emptyOfficeDBConnection();
+//
+//        Config::set("database.connections.OfficeDB", [
+//            'driver' => 'mysql',
+//            "host" => $officeDomain->domain_host,
+//            "database" => $officeDomain->office_db,
+//            "username" => $officeDomain->domain_username,
+//            "password" => $officeDomain->domain_password,
+//            "port" => 3306,
+//            'charset' => 'utf8',
+//            'collation' => 'utf8_general_ci',
+//        ]);
+//
+//        DB::purge('OfficeDB');
+//        DB::reconnect('OfficeDB');
+//
+//        return ['status' => 'success', 'message' => __("Successfully Connected"), 'office_id' => $office_id, 'office_domain' => $officeDomain];
     }
 
     public function emptyOfficeDBConnection()
